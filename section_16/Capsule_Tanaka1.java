04ce595a72942cf182c14d2a32a022e82973f8d4
ogihara0833 <140858355+ogihara0833@users.noreply.github.com>
2025-04-17 05:13:07 +0900

    アップロード

---
+++ section_16/Capsule_Tanaka1.java
@@ -0,0 +1,20 @@
+package text.section_16;
+
+public class Capsule_Tanaka1 {
+
+	//idフィールド（アクセス修飾子private）
+	private int id = 7;
+	
+	//
+	//コンストラクタ
+	Capsule_Tanaka1() {
+	}
+	
+	
+	
+	public int getId() {
+		return this.id = 45;
+	}
+	
+	
+}
---
+++ section_16/Capsule_Test1.java
@@ -0,0 +1,15 @@
+package text.section_16;
+
+public class Capsule_Test1 {
+	
+	public static void main(String[] args) {
+		Capsule_Tanaka1 tanaka = new Capsule_Tanaka1();
+		
+			int id = tanaka.getId();
+			System.out.println("idは" + id);
+			/*thisを使ってフィールド変数にアクセスする時は
+			 (this.フィールド変数)と記載する*/
+			
+			
+		}
+}

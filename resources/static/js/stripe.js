8d1ce409809ecdd2cb955864166ab4473ed786ab
ogihara0833 <140858355+ogihara0833@users.noreply.github.com>
2025-06-06 10:24:07 +0900

    Delete resources directory

--- resources/static/js/stripe.js
+++
@@ -1,8 +0,0 @@
-const stripe = Stripe('pk_test_51RTiYLB1nhrCgbjCIUnEeX8OuCxOX9KYbylADBbNmsr2htMP5UvMuYRgHQWFldm82m7ARKMzsfdx7e06YYbmiTHA00kufUVjWi');
-const paymentButton = document.querySelector('#paymentButton');
-
-paymentButton.addEventListener('click', () => {
- stripe.redirectToCheckout({
-   sessionId: sessionId
- })
-});
